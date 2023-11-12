3d0583c880966657f32494daf3fd44974d6419b0
LilianeTop <hallo@lilianetop.nl>
2023-11-12 17:32:37 +0100

    added mvc for customer

---
+++ src/main/java/nl/lilianetop/springframeworkmvc/models/Customer.java
@@ -0,0 +1,16 @@
+package nl.lilianetop.springframeworkmvc.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+@Data
+@Builder
+public class Customer {
+    private UUID id;
+    private String customerName;
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
---
+++ src/main/java/nl/lilianetop/springframeworkmvc/services/CustomerService.java
@@ -0,0 +1,12 @@
+package nl.lilianetop.springframeworkmvc.services;
+
+import nl.lilianetop.springframeworkmvc.models.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+
+    Customer getCustomerById(UUID id);
+    List<Customer> listCustomers();
+}
