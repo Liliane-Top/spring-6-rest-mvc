8ab4e6e501d2b61c3415ec8183637615d7519561
LilianeTop <hallo@lilianetop.nl>
2023-12-16 11:51:10 +0100

    fixed issue with running tests using h2

--- src/test/java/nl/lilianetop/springframeworkmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/nl/lilianetop/springframeworkmvc/bootstrap/BootstrapDataTest.java
@@ -8,10 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @Slf4j
 class BootstrapDataTest {
@@ -29,7 +26,7 @@ class BootstrapDataTest {
 
     @Test
     void callRun() throws Exception {
-        bootstrapData.run(null);
+        bootstrapData.run((String[]) null);
 
         assertThat(beerRepository.count()).isEqualTo(3);
         assertThat(customerRepository.count()).isEqualTo(3);
