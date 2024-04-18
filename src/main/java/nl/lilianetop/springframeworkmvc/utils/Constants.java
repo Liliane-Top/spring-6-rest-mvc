3b18d9b90665ddc9a048f984a5ec37caca059ad5
Liliane Top <hallo@lilianetop.nl>
2024-04-18 14:38:27 +0200

    changed url as consistent with other services, all works well

--- src/main/java/nl/lilianetop/springframeworkmvc/utils/Constants.java
+++ src/main/java/nl/lilianetop/springframeworkmvc/utils/Constants.java
@@ -1,8 +1,8 @@
 package nl.lilianetop.springframeworkmvc.utils;
 
 public class Constants {
-    public static final String CUSTOMER_URL = "/api/v1/customer/";
-    public static final String CUSTOMER_URL_ID = CUSTOMER_URL + "{customerId}";
-    public static final String BEER_URL = "/api/v1/beer/";
-    public static final String BEER_URL_ID = BEER_URL + "{beerId}";
+    public static final String CUSTOMER_URL = "/api/v1/customer";
+    public static final String CUSTOMER_URL_ID = CUSTOMER_URL + "/{customerId}";
+    public static final String BEER_URL = "/api/v1/beer";
+    public static final String BEER_URL_ID = BEER_URL + "/{beerId}";
 }
