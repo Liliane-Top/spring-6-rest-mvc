41df66f31b49bd968ed9ec5fa411463c5932fa78
Liliane Top <hallo@lilianetop.nl>
2024-04-26 16:08:47 +0200

    added openapi configuration

--- src/main/java/nl/lilianetop/springframeworkmvc/config/SpringSecurityConfig.java
+++ src/main/java/nl/lilianetop/springframeworkmvc/config/SpringSecurityConfig.java
@@ -5,17 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SpringSecurityConfig {
+    //to see all endpoint + openapi specs http://localhost:8081/swagger-ui/index.html
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs"),
+                                new AntPathRequestMatcher("/swagger-ui/**"),
+                                new AntPathRequestMatcher("/v3/api-docs/**"),
+                                new AntPathRequestMatcher("/swagger-ui.html"))
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
+
         return http.build();
     }
 
